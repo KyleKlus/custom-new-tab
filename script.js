a627cc46c64c702dfd1eb4687a6fd91d6e9a9cdd
Enkidu <kyle.klus@pm.me>
2023-02-07 00:05:30 +0100

    - Improved image loading time - Redone clock mechanisim

--- script.js
+++ script.js
@@ -1,37 +1,80 @@
+/** @format */
+
 // script.js
-// Load the background image and hide the loading screen
-var image = new Image();
-image.src = "https://source.unsplash.com/1920x1080/?nature";
-image.onload = function() {
-    console.log("test")
-    document.body.style.backgroundImage = "url('" + image.src + "')";
-    var content = document.getElementById("bookmark-list");
-    content.className = "show";
+// Load the background image from local storage
+const imageWidth = 1920;
+const imageHeight = 1080;
+const imageURL =
+  'https://source.unsplash.com/' + imageWidth + 'x' + imageHeight + '/?nature';
+const image = new Image();
+
+const imageData = localStorage.getItem('imgData');
+if (!imageData) {
+  image.src = imageURL;
+} else {
+  image.src = 'data:image/png;base64,' + imageData;
+  localStorage.removeItem('imgData');
+}
+
+image.onload = function () {
+  document.body.style.backgroundImage = "url('" + image.src + "')";
+  document.body.style.backgroundSize = 'cover';
+  document.body.style.backgroundRepeat = 'no-repeat';
+  document.body.style.backgroundPosition = 'center';
+  var content = document.getElementById('content');
+  content.className = 'show';
 };
 
-chrome.bookmarks.getTree(function(bookmarkTreeNodes) {
-    var list = document.getElementById("bookmark-list");
+// Init clock
+function refreshClock() {
+  const clock = document.getElementById('clock');
+  if (clock) {
+    const time = new Date();
+    clock.innerHTML = time.toLocaleTimeString('de-DE', {
+      hour: '2-digit',
+      minute: '2-digit',
+    });
+  }
+}
+this.refreshClock();
+
+setInterval(() => this.refreshClock(), 1000);
+
+// Load bookmarks
+if (chrome) {
+  chrome.bookmarks.getTree(function (bookmarkTreeNodes) {
+    var list = document.getElementById('bookmark-list');
     for (var i = 0; i < bookmarkTreeNodes[0].children[0].children.length; i++) {
       var node = bookmarkTreeNodes[0].children[0].children[i];
-      if(!node.url) continue
-      var li = document.createElement("li");
-      var a = document.createElement("a");
-        a.href = node.url;
-        a.innerText = node.title;
-        a.innerHTML = "<img src='https://s2.googleusercontent.com/s2/favicons?domain=" + node.url + "&sz=16' height='16' width='16' style='margin-right:10px; overflow: hidden; text-wrap:nowrap'>" + node.title;
+      if (!node.url) continue;
+      var li = document.createElement('li');
+      var a = document.createElement('a');
+      a.href = node.url;
+      a.innerText = node.title;
+      a.innerHTML =
+        "<img src='https://s2.googleusercontent.com/s2/favicons?domain=" +
+        node.url +
+        "&sz=16' height='16' width='16' style='margin-right:10px; overflow: hidden; text-wrap:nowrap'>" +
+        node.title;
       li.appendChild(a);
       list.appendChild(li);
     }
-});
+  });
+}
 
-// Refresh the background image every day
-setInterval(function() {
-  document.querySelector(".loader").style.display = "flex";
-  var image = new Image();
-  image.src = "https://source.unsplash.com/1920x1080/?nature";
-  image.onload = function() {
-    document.body.style.backgroundImage = "url('" + image.src + "')";
-    var content = document.getElementById("bookmark-list");
-    content.className = "show";
-  };
-}, 86400000);
+// Save a new background image to local storage
+const newImage = new Image();
+newImage.crossOrigin = 'anonymous';
+newImage.src = imageURL;
+newImage.onload = function () {
+  const canvas = document.createElement('canvas');
+  canvas.width = imageWidth;
+  canvas.height = imageHeight;
+  const ctx = canvas.getContext('2d');
+  ctx.drawImage(newImage, 0, 0);
+  var dataURL = canvas.toDataURL('image/png');
+  localStorage.setItem(
+    'imgData',
+    dataURL.replace(/^data:image\/(png|jpg);base64,/, '')
+  );
+};

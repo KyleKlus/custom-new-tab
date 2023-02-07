d1e2593f14f0f02f9de6a485dfe4899773520ed8
MajorEnkidu <kyle.klus.2@gmial.com>
2023-02-07 21:01:54 +0100

    - Fixed a bug

--- script.js
+++ script.js
@@ -7,57 +7,8 @@ const imageHeight = 1080;
 const imageURL =
   'https://source.unsplash.com/' + imageWidth + 'x' + imageHeight + '/?nature';
 const image = new Image();
-let imageData;
 let isRefreshNeeded = true; // TODO: only refresh if 6h old
 
-chrome.storage.local.get('imgData').then((result) => {
-  if (!result) {
-    image.src = imageURL;
-    return;
-  } else {
-    image.src = 'data:image/png;base64,' + result.imgData;
-    chrome.storage.local.remove('imgData');
-  }
-
-  image.onload = function () {
-    document.body.style.backgroundImage = "url('" + image.src + "')";
-    document.body.style.backgroundSize = 'cover';
-    document.body.style.backgroundRepeat = 'no-repeat';
-    document.body.style.backgroundPosition = 'center';
-    var content = document.getElementById('content');
-    content.className = 'show';
-
-    // Save a new background image to local storage
-    if (isRefreshNeeded) {
-      const newImage = new Image();
-      newImage.crossOrigin = 'anonymous';
-      newImage.src = imageURL;
-      newImage.onload = function () {
-        const canvas = document.createElement('canvas');
-        canvas.width = imageWidth;
-        canvas.height = imageHeight;
-        const ctx = canvas.getContext('2d');
-        ctx.drawImage(newImage, 0, 0);
-        var dataURL = canvas.toDataURL('image/png');
-        chrome.storage.local.set({
-          imgData: dataURL.replace(/^data:image\/(png|jpg);base64,/, ''),
-        });
-      };
-    }
-  };
-});
-
-// Init clock
-function refreshClock() {
-  const clock = document.getElementById('clock');
-  if (clock) {
-    const time = new Date();
-    clock.innerHTML = time.toLocaleTimeString('de-DE', {
-      hour: '2-digit',
-      minute: '2-digit',
-    });
-  }
-}
 this.refreshClock();
 
 setInterval(() => this.refreshClock(), 1000);
@@ -82,4 +33,61 @@ if (chrome) {
       list.appendChild(li);
     }
   });
+}
+
+chrome.storage.local
+  .get('imgData')
+  .then((result) => {
+    if (!result || !result.imgData) {
+      image.src = imageURL;
+      return;
+    } else {
+      image.src = 'data:image/png;base64,' + result.imgData;
+    }
+    image.onload = this.showImage;
+  })
+  .then(() => {
+    this.refreshStoredImage();
+  });
+
+// Init clock
+function refreshClock() {
+  const clock = document.getElementById('clock');
+  if (clock) {
+    const time = new Date();
+    clock.innerHTML = time.toLocaleTimeString('de-DE', {
+      hour: '2-digit',
+      minute: '2-digit',
+    });
+  }
+}
+
+function showImage() {
+  document.body.style.backgroundImage = "url('" + image.src + "')";
+  document.body.style.backgroundSize = 'cover';
+  document.body.style.backgroundRepeat = 'no-repeat';
+  document.body.style.backgroundPosition = 'center';
+  var content = document.getElementById('content');
+  content.className = 'show';
+}
+
+function refreshStoredImage() {
+  const newImage = new Image();
+  newImage.crossOrigin = 'anonymous';
+  newImage.src = imageURL;
+  newImage.onload = () => {
+    const canvas = document.createElement('canvas');
+    canvas.width = imageWidth;
+    canvas.height = imageHeight;
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(newImage, 0, 0);
+    var dataURL = canvas.toDataURL('image/png');
+
+    // Clear storage and save downloaded image
+    chrome.storage.local.clear().then((_) => {
+      chrome.storage.local.set({
+        imgData: dataURL.split(';base64,')[1],
+      });
+    });
+  };
 }
